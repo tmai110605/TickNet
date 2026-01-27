5987ec3917e7c76509cbe76de0351c1efc4024bd
_mqt. <23133071@student.hcmute.edu.vn>
2026-01-27 16:48:38 +0700

    Update TickNet_Dogs.py

--- TickNet_Dogs.py
+++ TickNet_Dogs.py
@@ -18,6 +18,28 @@ import torchvision.datasets
 from models.datasets import *
 from models.TickNet import *
 import writeLogAcc as wA
+class LabelSmoothingCrossEntropy(nn.Module):
+    """
+    NLL loss with label smoothing.
+    """
+    def __init__(self, smoothing=0.1):
+        """
+        Constructor for the LabelSmoothing module.
+        :param smoothing: label smoothing factor
+        """
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        assert smoothing < 1.0
+        self.smoothing = smoothing
+        self.confidence = 1. - smoothing
+
+    def forward(self, x, target):
+        # import pdb;pdb.set_trace()
+        logprobs = F.log_softmax(x, dim=-1)
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()#sys.path.append(str(Path('.').absolute().parent))
 
 def get_args():
     """
