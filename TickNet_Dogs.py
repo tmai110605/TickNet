af2cb7b7db71e5dc3236ba93ad8ef0f46e6af27f
_mqt. <23133071@student.hcmute.edu.vn>
2026-04-20 21:31:01 +0700

    Update TickNet_Dogs.py

--- TickNet_Dogs.py
+++ TickNet_Dogs.py
@@ -17,10 +17,31 @@ import torchvision.datasets
 #sys.path.append(str(Path('.').absolute().parent))
 from models.datasets import *
 from models.TickNet import *
-from models.cross_entropy import LabelSmoothingCrossEntropy
+
 import writeLogAcc as wA
 from checkmodel import print_model_stats
+class LabelSmoothingCrossEntropy(nn.Module):
+    """
+    NLL loss with label smoothing.
+    """
+    def __init__(self, smoothing=0.1):
+        """
+        Constructor for the LabelSmoothing module.
+        :param smoothing: label smoothing factor
+        """
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        assert smoothing < 1.0
+        self.smoothing = smoothing
+        self.confidence = 1. - smoothing
 
+    def forward(self, x, target):
+        # import pdb;pdb.set_trace()
+        logprobs = F.log_softmax(x, dim=-1)
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()#sys.path.append(str(Path('.').absolute().parent))
 def get_args():
     """
     Parse the command line arguments.
