3e48c91f89188423acc773a5b0d51a052cd8c5c6
_mqt. <23133071@student.hcmute.edu.vn>
2026-04-28 22:06:41 +0700

    Update Net3P_CIFAR10.py

--- Net3P_CIFAR10.py
+++ Net3P_CIFAR10.py
@@ -4,8 +4,7 @@ import argparse
 import sys
 import os
 #import inspect
-
-import torch.nn as nn
+import time
 import torch
 import torch.nn
 import torch.optim
@@ -13,55 +12,32 @@ import torch.optim.lr_scheduler
 import torch.utils.data
 import torchvision.transforms
 import torchvision.datasets
-class LabelSmoothingCrossEntropy(nn.Module):
-    """
-    NLL loss with label smoothing.
-    """
-    def __init__(self, smoothing=0.1):
-        """
-        Constructor for the LabelSmoothing module.
-        :param smoothing: label smoothing factor
-        """
-        super(LabelSmoothingCrossEntropy, self).__init__()
-        assert smoothing < 1.0
-        self.smoothing = smoothing
-        self.confidence = 1. - smoothing
-
-    def forward(self, x, target):
-        # import pdb;pdb.set_trace()
-        logprobs = F.log_softmax(x, dim=-1)
-        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
-        nll_loss = nll_loss.squeeze(1)
-        smooth_loss = -logprobs.mean(dim=-1)
-        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
-        return loss.mean()#sys.path.append(str(Path('.').absolute().parent))
+from checkmodel import print_model_stats
 #from pathlib import Path
 #sys.path.append(str(Path('.').absolute().parent))
-from models.TickNet import *
-#from models.NetTOP import *
+from models.datasets import *
+from models.TickNet import build_TickNet_BESTvg
 import writeLogAcc as wA
-from checkmodel import *
+
 def get_args():
     """
     Parse the command line arguments.
     """
-    parser = argparse.ArgumentParser(description='TickNet training script for CIFAR and fine-grained datasets.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    #parser.add_argument('-r', '--data-root', type=str, required=True, help='Dataset root path.')
+    parser = argparse.ArgumentParser(description='TickNet training script for cifar and StanfordDogs datasets.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)    
     parser.add_argument('-r', '--data-root', type=str, default='data', help='Dataset root path.')
     #parser.add_argument('-d', '--dataset', choices=['cifar10', 'cifar100', 'dogs'], required=True, help='Dataset name.')
-    parser.add_argument('-d', '--dataset', type=str, choices=['cifar10', 'cifar100', 'dogs'], default='cifar100', help='Dataset name.')
-    parser.add_argument('--download', action='store_true', help='Download the specified dataset before running the training.')
-    #parser.add_argument('-a', '--architecture', type=str, required=True, help='Model architecture name.')
-    parser.add_argument('-a', '--architecture', type=str, default='mobilenetv1_w1', help='Model architecture name.')
+    parser.add_argument('-d', '--dataset', type=str, choices=['cifar10', 'cifar100', 'dogs'], default='dogs', help='Dataset name.')
+    parser.add_argument('--download', action='store_true', help='Download the specified dataset before running the training.')    
     parser.add_argument('-g', '--gpu-id', default=1, type=int, help='ID of the GPU to use. Set to -1 to use CPU.')
     parser.add_argument('-j', '--workers', default=4, type=int, help='Number of data loading workers.')
-    parser.add_argument('-b', '--batch-size', default=128, type=int, help='Batch size.')        
+    parser.add_argument('-b', '--batch-size', default=64, type=int, help='Batch size.')        
     parser.add_argument('-e', '--epochs', default=200, type=int, help='Number of total epochs to run.')
     parser.add_argument('-l', '--learning-rate', default=0.1, type=float, help='Initial learning rate.')
     parser.add_argument('-s', '--schedule', nargs='+', default=[100, 150, 180], type=int, help='Learning rate schedule (epochs after which the learning rate should be dropped).')    
     parser.add_argument('-m', '--momentum', default=0.9, type=float, help='SGD momentum.')
-    parser.add_argument('-w', '--weight-decay', default=1e-4, type=float, help='SGD weight decay.')
-    #parser.add_argument('--alpha', default=0.1, type=float, help='BSConv-S weighting coefficient for the regularization loss.')
+    parser.add_argument('-w', '--weight-decay', default=1e-4, type=float, help='SGD weight decay.')    
+    parser.add_argument('--evaluate', dest='evaluate', action='store_true',
+                        help='evaluate model on validation set')
     return parser.parse_args()
 
 
@@ -76,11 +52,7 @@ def get_device(args):
     
 
 def get_data_loader(args, train):
-    """
-    Return the data loader for the given arguments.
-    """
     if args.dataset in ('cifar10', 'cifar100'):
-        # select transforms based on train/val
         if train:
             transform = torchvision.transforms.Compose([
                 torchvision.transforms.RandomCrop(32, padding=4),
@@ -91,22 +63,21 @@ def get_data_loader(args, train):
             transform = torchvision.transforms.Compose([
                 torchvision.transforms.ToTensor(),
             ])
-    
-        # cifar10 vs. cifar100
         if args.dataset == 'cifar10':
             dataset_class = torchvision.datasets.CIFAR10
         else:
             dataset_class = torchvision.datasets.CIFAR100
             
     elif args.dataset in ('dogs',):
-        # select transforms based on train/val
         if train:
             transform = torchvision.transforms.Compose([
                 torchvision.transforms.Resize(size=(256, 256)),
                 torchvision.transforms.RandomCrop(224),
                 torchvision.transforms.RandomHorizontalFlip(),
                 torchvision.transforms.ColorJitter(0.4),
-                torchvision.transforms.ToTensor()
+                torchvision.transforms.RandAugment(), 
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.RandomErasing(p=0.2) 
             ])
         else:
             transform = torchvision.transforms.Compose([
@@ -115,18 +86,16 @@ def get_data_loader(args, train):
                 torchvision.transforms.ToTensor()
             ])
     
-        dataset_class = bsconv.datasets.StanfordDogs
-    
+        dataset_class = StanfordDogs # Import từ models.datasets
     else:
         raise NotImplementedError('Can\'t determine data loader for dataset \'{}\''.format(args.dataset))
     
-    # trigger download only once
-    if args.download:
-        dataset_class(root=args.data_root, train=train, download=True, transform=transform)
-
-    # instantiate dataset class and create data loader from it
-    dataset = dataset_class(root=args.data_root, train=train, download=True, transform=transform)
-    return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True if train else False, num_workers=args.workers)    
+    # -------------------------------------------------------------------------
+  
+    # -------------------------------------------------------------------------
+    dataset = dataset_class(root='./data', train=train, download=False, transform=transform)
+    
+    return torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True if train else False, num_workers=args.workers)
 
 
 def calculate_accuracy(output, target):
@@ -158,8 +127,8 @@ def run_epoch(train, data_loader, model, criterion, optimizer, n_epoch, args, de
         target = target.to(device)
 
         output = model(images)
-        loss = criterion(output, target)
-        
+        loss = criterion(output, target)        
+
         # record loss and measure accuracy
         loss_item = loss.item()
         losses.append(loss_item)
@@ -172,8 +141,8 @@ def run_epoch(train, data_loader, model, criterion, optimizer, n_epoch, args, de
             loss.backward()
             optimizer.step()
     
-      #  if (n_batch % 10) == 0:
-          #  print('[{}]  epoch {}/{},  batch {}/{},  loss_{}={:.5f},  acc_{}={:.2f}%'.format('train' if train else ' val ', n_epoch + 1, args.epochs, n_batch + 1, batch_count, "train" if train else "val", loss_item, "train" if train else "val", 100.0 * acc))
+        #if (n_batch % 10) == 0:
+            #print('[{}]  epoch {}/{},  batch {}/{},  loss_{}={:.5f},  acc_{}={:.2f}%'.format('train' if train else ' val ', n_epoch + 1, args.epochs, n_batch + 1, batch_count, "train" if train else "val", loss_item, "train" if train else "val", 100.0 * acc))
     
     return (sum(losses) / len(losses), sum(accs) / len(accs))
             
@@ -184,35 +153,34 @@ def main():
     """
     args = get_args()
     print('Command: {}'.format(' '.join(sys.argv)))
-    #args.gpu_id = 0
+    #args.gpu_id = 1
     device = get_device(args)
     print('Using device {}'.format(device))
-    
+    g=4
+    op='top'
     # print model with parameter and FLOPs counts    
     torch.autograd.set_detect_anomaly(True)     
     
-    arr_typesize_groups = [2,1,4]
-    
-    for typesize in arr_typesize_groups:  
-        strmode = 'NetTOP_used_groups_' + str(typesize)  
-        pathout = './checkpoints/CIFAR10_NetTOP/' + strmode
+    #arr_typesize = ['large', 'small']
+    arr_typesize = ['small']
+    for typesize in arr_typesize:    
+        strmode = 'StanfordDogs_TickNet_' + typesize + '_MAF'  
+        pathout = './checkpoints/' + strmode
         filenameLOG = pathout + '/' + strmode + '.txt'
         if not os.path.exists(pathout):
             os.makedirs(pathout)
         # get model
-         
-        #model = build_NetTOP(10, cifar=True,groups=typesize)
-        model = TickNet_BEST(100, cifar=True)
+        
+       # model =model = build_TickNet_BESTvg(120, cifar=False)
+        model = build_TickNet_BESTvg(120, cifar=False, g=4, operator=op)
         model = model.to(device)
-        #print_model_stats(model, input_size=(3,32,32))
+        #print_model_stats(model, input_size=(3, 224, 224))
         print(model)
         
         print('Number of model parameters: {}'.format(
         sum([p.data.nelement() for p in model.parameters()])))
-        
+    
         # define loss function and optimizer
-        train_loss_fn = LabelSmoothingCrossEntropy(smoothing=0.1).to(device)
-        #validate_loss_fn = nn.CrossEntropyLoss().to(device)
         criterion = torch.nn.CrossEntropyLoss().to(device)
         optimizer = torch.optim.SGD(params=model.parameters(), lr=args.learning_rate, momentum=args.momentum, weight_decay=args.weight_decay)
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=args.schedule, gamma=0.1)
@@ -221,6 +189,22 @@ def main():
         train_loader = get_data_loader(args=args, train=True)
         val_loader = get_data_loader(args=args, train=False)
         
+        if args.evaluate:
+            pathcheckpoint = "./checkpoints/StanfordDogs/small/model_best.pth"
+            if os.path.isfile(pathcheckpoint):
+                print("=> loading checkpoint '{}'".format(pathcheckpoint))
+                checkpoint = torch.load(pathcheckpoint)
+                model.load_state_dict(checkpoint['model_state_dict'])                
+                del checkpoint
+            else:
+                print("=> no checkpoint found at '{}'".format(pathcheckpoint))
+                return
+            m = time.time()
+            (loss_val, acc_val) = run_epoch(train=False, data_loader=val_loader, model=model, criterion=criterion, optimizer=None, n_epoch=0, args=args, device=device)
+            print('[{}], loss_{}={:.5f},  acc_{}={:.2f}%'.format(' validating: ', "val", loss_val, "val", 100.0 * acc_val))
+            n = time.time()
+            print((n-m)/3600)
+            return
         # for each epoch...
         acc_val_max = None
         acc_val_argmax = None
@@ -229,19 +213,18 @@ def main():
             print('Starting epoch {}/{},  learning_rate={}'.format(n_epoch + 1, args.epochs, current_learning_rate))
             
             # train
-            #(loss_train, acc_train) = run_epoch(train=True, data_loader=train_loader, model=model, criterion=criterion, optimizer=optimizer, n_epoch=n_epoch, args=args, device=device)
-            (loss_train, acc_train) = run_epoch(train=True, data_loader=train_loader, model=model, criterion=train_loss_fn, optimizer=optimizer, n_epoch=n_epoch, args=args, device=device)
-            
+            (loss_train, acc_train) = run_epoch(train=True, data_loader=train_loader, model=model, criterion=criterion, optimizer=optimizer, n_epoch=n_epoch, args=args, device=device)
+    
             # validate
             (loss_val, acc_val) = run_epoch(train=False, data_loader=val_loader, model=model, criterion=criterion, optimizer=None, n_epoch=n_epoch, args=args, device=device)
             if (acc_val_max is None) or (acc_val > acc_val_max):
                 acc_val_max = acc_val
                 acc_val_argmax = n_epoch
                 torch.save({"model_state_dict": model.state_dict()}, pathout + '/' + 'checkpoint_epoch{:>04d}_{:.2f}.pth'.format(n_epoch + 1,100.0 * acc_val_max))
-        
+    
             # adjust learning rate
             scheduler.step()
-        
+    
             # save the model weights
             #torch.save({"model_state_dict": model.state_dict()}, 'checkpoint_epoch{:>04d}.pth'.format(n_epoch + 1))
             
