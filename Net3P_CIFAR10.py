d130886b705bb2fc43bafb138b2fa822f129f937
_mqt. <23133071@student.hcmute.edu.vn>
2026-04-29 12:09:13 +0700

    Update Net3P_CIFAR10.py

--- Net3P_CIFAR10.py
+++ Net3P_CIFAR10.py
@@ -26,7 +26,7 @@ def get_args():
     parser = argparse.ArgumentParser(description='TickNet training script for cifar and StanfordDogs datasets.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)    
     parser.add_argument('-r', '--data-root', type=str, default='data', help='Dataset root path.')
     #parser.add_argument('-d', '--dataset', choices=['cifar10', 'cifar100', 'dogs'], required=True, help='Dataset name.')
-    parser.add_argument('-d', '--dataset', type=str, choices=['cifar10', 'cifar100', 'dogs'], default='cifar10', help='Dataset name.')
+    parser.add_argument('-d', '--dataset', type=str, choices=['cifar10', 'cifar100', 'dogs'], default='cifar100', help='Dataset name.')
     parser.add_argument('--download', action='store_true', help='Download the specified dataset before running the training.')    
     parser.add_argument('-g', '--gpu-id', default=1, type=int, help='ID of the GPU to use. Set to -1 to use CPU.')
     parser.add_argument('-j', '--workers', default=4, type=int, help='Number of data loading workers.')
@@ -172,7 +172,7 @@ def main():
         # get model
         
        # model =model = build_TickNet_BESTvg(120, cifar=False)
-        model = build_TickNet_BESTvg(10, cifar=True, g=4, operator=op)
+        model = build_TickNet_BESTvg(100, cifar=True, g=4, operator=op)
         model = model.to(device)
         #print_model_stats(model, input_size=(3, 224, 224))
         #print(model)
